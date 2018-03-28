d68512188f1a2648a9a4848ad1de4d3cb7dc0438
Federico Vera <fede@riddler.com.ar>
2018-03-28 16:22:39 -0300

    Remove unused import

--- src/main/java/com/dkt/graphics/canvas/Canvas.java
+++ src/main/java/com/dkt/graphics/canvas/Canvas.java
@@ -36,7 +36,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.text.DecimalFormat;
