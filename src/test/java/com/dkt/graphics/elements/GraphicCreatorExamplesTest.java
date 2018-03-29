524d9c458ac3dfc80f5fe514286539125d02b4d0
Federico Vera <fede@riddler.com.ar>
2018-03-29 17:25:23 -0300

    Remove messy test, need to find better way of testing this.

--- src/test/java/com/dkt/graphics/elements/GraphicCreatorExamplesTest.java
+++ src/test/java/com/dkt/graphics/elements/GraphicCreatorExamplesTest.java
@@ -42,122 +42,24 @@ public class GraphicCreatorExamplesTest {
             "for1 0 10 300 \"stroke 3; linec 0 %1$d %1$d 0\"\n" +
             "for1 0 10 300 \"stroke 3; color 0 %1$d %1$d; linec %1$d 300 300 %1$d\"");
         Graphic g2 = new Graphic();
-        BasicStroke s = new BasicStroke(3);
-        for (int i = 0; i < 300; i += 10) {
+        Graphic g3 = new Graphic();
+        BasicStroke s = new BasicStroke(3f, BasicStroke.CAP_SQUARE,BasicStroke.JOIN_BEVEL);
+        for (int i = 0; i <= 300; i += 10) {
             GLine l = new GLine(0, i, i, 0);
             l.setStroke(s);
-            g2.add(l);
+            g3.add(l);
         }
-        for (int i = 0; i < 300; i += 10) {
+        g2.add(g3);
+        g3 = new Graphic();
+        for (int i = 0; i <= 300; i += 10) {
             GLine l = new GLine(i, 300, 300, i);
             l.setStroke(s);
             l.setPaint(new Color(0, Math.min(i, 255), Math.min(i, 255)));
-            g2.add(l);
+            g3.add(l);
         }
+        g2.add(g3);
+        assertEquals(g.getCount(), g2.getCount());
         assertEquals(g, g2);
     }
     
-    @Test
-    @DisplayName("Examples->Lines->Lines 2") 
-    public void testLines2() {
-        GraphicCreator gc = new GraphicCreator();
-        Graphic g = gc.parse(
-            "stroke 0.5\n" +
-            "color 128 160 160\n" +
-            "linec 0 150 0 -150\n" +
-            "linec -150 0 150 0\n" +
-            "\n" +
-            "# Inner lines\n" +
-            "for2 200 -10 0 10 10 200 \"linec 0  %d  %d 0\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec 0  %d -%d 0\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec 0 -%d  %d 0\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec 0 -%d -%d 0\"\n" +
-            "\n" +
-            "color 100 100 200\n" +
-            "# Outter lines\n" +
-            "for2 200 -10 0 10 10 200 \"linec  200  %d  %d  200\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec -200  %d -%d  200\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec  200 -%d  %d -200\"\n" +
-            "for2 200 -10 0 10 10 200 \"linec -200 -%d -%d -200\""
-        );
-        Graphic g2 = new Graphic();
-        BasicStroke s = new BasicStroke(0.5f, BasicStroke.CAP_SQUARE,BasicStroke.JOIN_BEVEL);
-        Color c = new Color(128, 160, 160);
-        GLine l1 = new GLine(0, 150, 0, -150);
-        GLine l2 = new GLine(-150, 0, 150, 0);
-        l1.setPaint(c);
-        l2.setPaint(c);
-        l1.setStroke(s);
-        l2.setStroke(s);
-        g2.add(l1);
-        g2.add(l2);
-        
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(0, i, i, 0);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(0, i, -i, 0);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(0, -i, i, 0);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(0, -i, -i, 0);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        c = new Color(100, 100, 200);
-        
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(200, i, i, 200);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(-200, i, -i, 200);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(200, -i, i, -200);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        for (int i = 200; i >= 0; i -= 10) {
-            for (int j = 10; j < 200; j += 10) {
-                GLine l = new GLine(-200, -i, -i, -200);
-                l.setPaint(c);
-                l.setStroke(s);
-                g2.add(l);
-            }
-        }
-        assertEquals(g, g2);
-    }
 }
